18a44142372725e3a68404f20bdbc80554b8f2c0
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-28 12:44:20 +0100

    snack 5 finito

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,21 +1,22 @@
 const squads = [
     {
-        "nome": "VolleyRoma",
-        "puntiSegnati": [],
-        "falliSubiti": []
+        "nome": "VolleyRoma"
     },
     {
-        "nome": "VolleyMilano",
-        "puntiSegnati": [],
-        "falliSubiti": []
+        "nome": "VolleyMilano"
     },
     {
-        "nome": "VolleyBologna",
-        "puntiSegnati": [],
-        "falliSubiti": []
+        "nome": "VolleyBologna"
     },
 ]
 
 const generateRandomNum = (maxLimit, minLimit) => {
     return Math.floor(Math.random() * maxLimit + minLimit);
 }
+
+squads.forEach((item) => {
+    item.puntiSegnati = generateRandomNum(100, 0); 
+    item.falliSubiti = generateRandomNum(10, 0);
+})
+
+console.log(squads);
